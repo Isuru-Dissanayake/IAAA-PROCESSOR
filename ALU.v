786c581dcda74caa9860541e3b5500bd43518bca
theamaya <minikiraniamaya@gmail.com>
2020-12-29 23:35:53 +0530

    ALU updated

---
+++ ALU.v
@@ -0,0 +1,70 @@
+//`timescale 1ns/1ns
+
+module IR( 
+	input Clock,
+  	input [19:0] WRDec_out,
+	input [15:0] MIDR_out, 
+	output reg [3:0] IR_out); 
+
+always @(posedge Clock) 
+	begin 
+      if (WRDec_out == 20'b10000000000000000000 || WRDec_out == 20'b11111111111111111111) 
+		IR_out <= MIDR_out[3:0]; 
+	end  
+ 
+endmodule
+
+module ALU(
+
+input Clock,
+input [15:0] In_1, // DATA PATH A(PRIMARY)
+input [15:0] In_2, // DATA PATH B
+input [3:0] ALUOp,
+output reg [15:0] ALUOut,
+output reg[0:0] Z = 0,
+output reg[0:0] Y = 0);
+
+
+  always @(posedge Clock)
+begin
+
+
+  if(ALUOp == 4'd1)  //ADD
+	ALUOut <= In_1 + In_2;
+
+  else if(ALUOp == 4'd2) //ADD1
+	ALUOut <= In_1 + 1;
+	
+  else if(ALUOp == 4'd3)  // SUB
+	begin
+	if(In_1 == In_2)
+      begin
+		Z<=1'b1;
+      	ALUOut <= In_1 - In_2;
+      end
+	else if(In_1 >> In_2)
+		Y<=1'b1;
+	else
+		ALUOut <= In_1 - In_2;
+	end
+	
+  else if(ALUOp == 4'd4) // SUB1
+	ALUOut <= In_1 - 1;
+  
+  else if(ALUOp == 4'd5) //MUL
+	ALUOut <= In_1 * In_2;
+
+  // else if(ALUop == 4d'6) // ROOF
+// 	ALUOut <= 
+
+  else if(ALUOp == 4'd7) // FLOOR
+	ALUOut <= In_1 / In_2;
+
+
+
+  else if(ALUOp == 4'd8) // MOD
+	ALUOut <= In_1 % In_2;
+
+end
+
+endmodule
---
+++ testbenches/ALU_testbench.v
@@ -0,0 +1,110 @@
+// Code your testbench here
+// or browse Examples
+`timescale 1ns/1ns
+
+module testbench();
+  reg clk;
+  reg [19:0] WRDec_out;
+  reg [15:0] MIDR_out;
+  wire [3:0] IR_out;
+  
+  reg [15:0] A_BUS;
+  reg [15:0] B_BUS;
+  reg [3:0] ALUOp;
+  wire [15:0] ALUOut;
+  wire [0:0] Z;
+  wire [0:0] Y;
+  
+  
+  IR ir(
+    .Clock (clk), 
+    .WRDec_out(WRDec_out), 
+    .MIDR_out(MIDR_out), 
+    .IR_out(IR_out));
+  
+  ALU alu(
+    .Clock(clk),
+    .In_1(A_BUS),
+    .In_2(B_BUS),
+    .ALUOp(ALUOp),
+    .ALUOut(ALUOut),
+    .Z(Z),
+    .Y(Y)
+  );
+  
+  always #5 clk= ~clk;
+  
+  initial begin
+    
+    clk=0; 
+    
+    A_BUS=16'b0000000000000101;
+    B_BUS=16'b0000000000000011;
+    ALUOp=4'd1;
+    #10 
+    $display("ALUOp=%b", ALUOp);
+    $display("ALUOut=%b", ALUOut);
+    $display("Z=%b", Z);
+    $display("Y=%b", Y);
+    
+    ALUOp=4'd2;
+    #10 
+    $display("ALUOp=%b", ALUOp);
+    $display("ALUOut=%b", ALUOut);
+    $display("Z=%b", Z);
+    $display("Y=%b", Y);
+        
+    ALUOp=4'd3;
+    #10 
+    $display("ALUOp=%b", ALUOp);
+    $display("ALUOut=%b", ALUOut);
+    $display("Z=%b", Z);
+    $display("Y=%b", Y);
+
+    ALUOp=4'd4;
+    #10 
+    $display("ALUOp=%b", ALUOp);
+    $display("ALUOut=%b", ALUOut);
+    $display("Z=%b", Z);
+    $display("Y=%b", Y);
+        
+    ALUOp=4'd5;
+    #10 
+    $display("ALUOp=%b", ALUOp);
+    $display("ALUOut=%b", ALUOut);
+    $display("Z=%b", Z);
+    $display("Y=%b", Y);
+
+    ALUOp=4'd7;
+    #10 
+    $display("ALUOp=%b", ALUOp);
+    $display("ALUOut=%b", ALUOut);
+    $display("Z=%b", Z);
+    $display("Y=%b", Y);
+        
+    ALUOp=4'd8;
+    #10 
+    $display("ALUOp=%b", ALUOp);
+    $display("ALUOut=%b", ALUOut);
+    $display("Z=%b", Z);
+    $display("Y=%b", Y);
+    
+    /*WRDec_out=20'b00000000000000000000; 
+    MIDR_out=16'b0101010101010101;
+    #10 $display("ir_out=%b", IR_out);
+    
+    WRDec_out=20'b10000000000000000000; 
+    MIDR_out=16'b0101010101010101;
+    #10 $display("ir_out=%b", IR_out);
+    
+    WRDec_out=20'b11111111111111111111; 
+    MIDR_out=16'b0000000000000000;
+	#10 $display("ir_out=%b", IR_out);
+    
+    WRDec_out=20'b00000000000000000000;  
+    MIDR_out=16'b0101010101010101;
+	#10 $display("ir_out=%b", IR_out);*/
+    
+    
+  end
+endmodule
